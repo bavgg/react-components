801e60cc946728ad80a4335467f18694bc4798ef
plunaden <plunaden@ip-192-168-0-133.ap-southeast-2.compute.internal>
2024-02-08 18:40:23 +0800

    searchbar autocomplete component added.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import SearchbarAutocomplete from './components/SearchbarAutocomplete'
+
+function App() {
+
+  return (
+    <>
+      <SearchbarAutocomplete/>
+    </>
+  )
+}
+
+export default App
